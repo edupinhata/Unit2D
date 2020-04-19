87bccce4303d7064a0959c44c5d5993105742690
Eduardo Pinhata <edupinhata@gmail.com>
2020-04-19 13:51:38 -0400

    Add Projects

---
+++ Intro_Setup/Hello World/Assets/HelloWorld.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    int min, max, guess;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame(){
+        max = 1000;
+        min = 1;
+        guess = 500;
+        Debug.Log("Choose a number between " + min + " and " + max + ".");
+        Debug.Log("Arrow Up = Number is Higher, Arrow Down = Number is Lower, Enter = Correct");
+        Debug.Log("First guess is: " + guess);
+        max += 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // The number is higher than the guess
+        if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            min = guess;
+            NextGuess();
+        }
+        // The number is lower than the guess
+        if (Input.GetKeyDown(KeyCode.DownArrow)){
+            max = guess;
+            NextGuess(); 
+        }
+        //The guess is right
+        if (Input.GetKeyDown(KeyCode.Return)){
+            Debug.Log("Seu número é " + guess);
+            StartGame();
+        }
+
+    }
+
+    void NextGuess()
+    {
+        guess =  (max + min) / 2;
+        Debug.Log("A nova guess é: " + guess);
+    }
+
+
+}
---
+++ TextAdventure/TextAdventure/Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+
+    State currentState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentState = startingState;
+        textComponent.text = currentState.GetStateStory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        var nextStates = currentState.GetNextStates();
+
+        for (int state_idx=0; state_idx<nextStates.Length; state_idx++) {
+            if ( Input.GetKeyDown(KeyCode.Alpha1 + state_idx)) { 
+                changeState(nextStates, state_idx);
+			}
+		}
+    }
+
+    private void changeState(State[] nextStates, int nextState)
+    {
+        currentState = nextStates[nextState];
+        textComponent.text = currentState.GetStateStory();
+    }
+}
